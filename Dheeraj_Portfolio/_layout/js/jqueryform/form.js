841af874cfdcc1a3fe5c884d434471a702e4ef04
Dheerajportfolio <dheerajmarripelli@dheerajs-mbp.home>
2017-05-03 22:51:16 -0400

    My Github Portfolio

---
+++ Dheeraj_Portfolio/_layout/js/jqueryform/form.js
@@ -0,0 +1,35 @@
+$(function(){
+			var email = $("#email");
+			var name = $("#name");
+
+			function validate(field){
+				if(field.val().length === 0){
+					field.removeClass().addClass("error");
+					field.next().removeClass().addClass("icon-warning-sign");
+				}else{
+					field.removeClass().addClass("success");
+					field.next().removeClass().addClass("icon-ok");
+				}
+				return field;
+			}
+
+			$('input').blur(function(){
+				validate($(this));
+			});
+
+			$("button").on("click" , function(){
+				validate(email);
+				validate(name);
+				if($(email).val().length === 0 || $(name).val().length === 0)
+					$(this).removeClass().addClass("submit-error");
+				else{
+					$(this).removeClass().addClass("submit-success");
+				}
+				window.setTimeout(function(){
+					console.log("done");
+					$("button").removeClass();
+				}, 3000)
+				return false;
+			});
+			
+		});
